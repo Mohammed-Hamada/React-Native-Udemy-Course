0c446f0b218a50da074c4e262cdc94717f526b4c
Mohammed-Hamada <mohammed3rbio@gmail.com>
2022-12-09 00:17:26 +0200

    Change useState() to useReducer()

--- src/screens/ManualColorGenerator.jsx
+++ src/screens/ManualColorGenerator.jsx
@@ -1,64 +1,52 @@
-import { FlatList, Text, View } from 'react-native';
-import React, { useState } from 'react';
+import { Text, View } from 'react-native';
+import React, { useReducer } from 'react';
 import ControlColorComponent from '../Components/ControlColorComponent';
 
-const COLOR_INCREMENT = 20;
+const reducer = (state, action) => {
+  // state: {red: number, green: number, blue: number}
+  // action: { type: 'red' || 'green' || 'blue', payload: 15 || -15}
+  switch (action.type) {
+    case 'change_red':
+      // Never going to do: state.red = state.red + 15;
+      return state.red + action.payload >= 255 || state.red + action.payload < 0
+        ? state
+        : { ...state, red: state.red + action.payload };
+    case 'change_green':
+      return state.green + action.payload >= 255 ||
+        state.green + action.payload < 0
+        ? state
+        : { ...state, green: state.green + action.payload };
+    case 'change_blue':
+      return state.blue + action.payload >= 255 || state.blue + action.payload < 0
+        ? state
+        : { ...state, blue: state.blue + action.payload };
+  }
+};
 
 const ManualColorGenerator = () => {
-  const [red, setRed] = useState(0);
-  const [green, setGreen] = useState(0);
-  const [blue, setBlue] = useState(0);
-
-  const setColor = (color, change) => {
-    // color  => "red", "green", "blue"
-    // change => +15, -15
-
-    switch (color) {
-      case 'red':
-        red + change > 255 || red + change < 0 ? null : setRed(red + change);
-        break;
-      case 'green':
-        green + change > 255 || green + change < 0
-          ? null
-          : setGreen(green + change);
-        break;
-      case 'blue':
-        blue + change > 255 || blue + change < 0
-          ? null
-          : setBlue(blue + change);
-        break;
-    }
-  };
-
-  const COLORS = [
-    {
-      color: 'Red',
-      onIncrease: () => setColor('red', COLOR_INCREMENT),
-      onDecrease: () => setColor('red',  -COLOR_INCREMENT),
-    },
-    {
-      color: 'Green',
-      onIncrease: () => setColor('green', COLOR_INCREMENT),
-      onDecrease: () => setColor('green', -COLOR_INCREMENT),
-    },
-    {
-      color: 'Blue',
-      onIncrease: () => setColor('blue', COLOR_INCREMENT),
-      onDecrease: () => setColor('blue', -COLOR_INCREMENT),
-    },
-  ];
-
+  const [state, dispatch] = useReducer(reducer, {
+    red: 0,
+    green: 0,
+    blue: 0,
+  });
+  const { red, green, blue } = state;
+  console.log(state);
   return (
     <View>
-      <FlatList
-        data={COLORS}
-        renderItem={({ item }) => (
-          <ControlColorComponent
-            onIncrease={item.onIncrease}
-            onDecrease={item.onDecrease}
-            colorName={item.color}
-          />
-        )}
+      <ControlColorComponent
+        onIncrease={() => dispatch({ type: 'change_red', payload: 20 })}
+        onDecrease={() => dispatch({ type: 'change_red', payload: -20 })}
+        colorName="Red"
+      />
+      <ControlColorComponent
+        onIncrease={() => dispatch({ type: 'change_green', payload: 20 })}
+        onDecrease={() => dispatch({ type: 'change_green', payload: -20 })}
+        colorName="Green"
+      />
+      <ControlColorComponent
+        onIncrease={() => dispatch({ type: 'change_blue', payload: 20 })}
+        onDecrease={() => dispatch({ type: 'change_blue', payload: -20 })}
+        colorName="Blue"
       />
       <View
         style={{
